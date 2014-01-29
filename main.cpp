a5b050b40aa1e00b145d4008e899b87ead8546e1
gh2k <gh2k@monkor.com>
2014-01-29 15:15:23 +0000

    Fix annoying typo

--- main.cpp
+++ main.cpp
@@ -805,7 +805,7 @@ int main(int argc, char **argv)
 	puts("Tips: 1gh2k13JzLTxDz2vmo8RH7HcjTLLg5kdc ;-)");
 	puts("");
 
-	printf("Staring %d threads. Hang on to your hats...\n", threadcount);
+	printf("Starting %d threads. Hang on to your hats...\n", threadcount);
 	printf("Sending payouts to %s\n", btc.data());
 
 	std::list<std::thread *> threads;
